d081781a83881557590378b1ce77f46258d3aeb1
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-16 22:26:48 -0600

    added navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./styles.css"
+
+function NavTabs() {
+  const location = useLocation();
+  // function burger() {
+  //   var burger = document.querySelector(".burger");
+  //   var menu = document.querySelector("#" + burger.dataset.target);
+  //   burger.addEventListener("click", function () {
+  //     burger.classList.toggle("is-active");
+  //     menu.classList.toggle("is-active");
+  //   });
+  // };
+  // burger()
+
+  return (
+    <div>
+      <nav className="navbar level" id="navBurger">
+        <div className="navbar-brand">
+          <div className="navbar-burger burger" data-target="navbarMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <div className="navbar-menu level-right" id="navbarMenu">
+          <div className="navbar-end">
+            <div className="navbar-item level-item">
+              <Link
+                to="/"
+                className={
+                  location.pathname === "/"
+                    ? "navbar-item active"
+                    : "navbar-item"
+                }
+              >
+                Home
+              </Link>
+            </div>
+            <div className="navbar-item level-item">
+              <Link
+                to="/about"
+                className={
+                  location.pathname === "/about"
+                    ? "navbar-item active"
+                    : "navbar-item"
+                }
+              >
+                About
+              </Link>
+            </div>
+            <div className="navbar-item level-item">
+              <Link
+                to="/portfolio"
+                className={
+                  location.pathname === "/portfolio"
+                    ? "navbar-item active"
+                    : "navbar-item"
+                }
+              >
+                Portfolio
+              </Link>
+            </div>
+            <div className="navbar-item level-item">
+              <Link
+                to="/contact"
+                className={
+                  location.pathname === "/contact"
+                    ? "navbar-item active"
+                    : "navbar-item"
+                }
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavTabs;
