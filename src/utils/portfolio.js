68ed6032d6ab2e73108d6a9179b38833ca18fe20
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-16 22:29:19 -0600

    added portfolio items

---
+++ src/utils/portfolio.js
@@ -0,0 +1,86 @@
+const Items = [
+  {
+    id: 8,
+    title: "Wonderful Workday Planner",
+    image: "/assets/images/day-planner-screenshot.png",
+    alt: "day planner screenshot",
+    deployed: "https://arnette110.github.io/Wonderful-Workday-Planner/",
+    source_code: "https://github.com/Arnette110/Wonderful-Workday-Planner",
+  },
+  {
+    id: 7,
+    title: "Wild Weather App",
+    image:
+      "/assets/images/2020-01-04 20_46_40-Wild Weather App-with-inputs.png",
+    alt: "weather app screenshot",
+    deployed: "https://arnette110.github.io/Wild-Weather-App/",
+    source_code: "https://github.com/Arnette110/Wild-Weather-App",
+  },
+  {
+    id: 6,
+    title: "Scrabble Party App",
+    image: "/assets/images/definition-soundcloud.png",
+    alt: "scrabble party app screenshot",
+    deployed: "https://arnette110.github.io/Scrabble-Party-App/",
+    source_code: "https://github.com/Arnette110/Scrabble-Party-App",
+  },
+  {
+    id: 5,
+    title: "PDF Generator",
+    image: "/assets/images/profile-generator.gif",
+    alt: "PDF generator gif",
+    deployed: "",
+    source_code: "https://github.com/Arnette110/PDF-Generator",
+  },
+  {
+    id: 4,
+    title: "Dynamic Team Generator",
+    image: "/assets/images/team-generator.gif",
+    alt: "team generator gif",
+    deployed: "",
+    source_code: "https://github.com/Arnette110/Dynamic-Team-Generator",
+  },
+  {
+    id: 3,
+    title: "Notable Note Taker",
+    image: "/assets/images/2020-02-10 18_00_45-Note Taker.png",
+    alt: "note taker app screenshot",
+    deployed: "https://agile-peak-63140.herokuapp.com/",
+    source_code: "https://github.com/Arnette110/Notable-Note-Taker",
+  },
+  {
+    id: 2,
+    title: "GalactChris Devourer Of Burgers",
+    image: "/assets/images/screenshot-GalactChris Devourer Of Burgers.png",
+    alt: "burger app screenshot",
+    deployed: "https://sheltered-citadel-94305.herokuapp.com/",
+    source_code:
+      "https://github.com/Arnette110/GalactChris-devourer-of-burgers",
+  },
+  {
+    id: 1,
+    title: "Stock Watcher",
+    image: "/assets/images/2020-03-18 15_07_28-Stock Watcher!.png",
+    alt: "stock watcher app",
+    deployed: "https://quiet-scrubland-40925.herokuapp.com/",
+    source_code: "https://github.com/Arnette110/stock-shares-viewer",
+  },
+  {
+    id: 10,
+    title: "Caching Budgets",
+    image: "/assets/images/2020-04-16 20_19_17-Caching-Budgets.png",
+    alt: "caching budgets app",
+    deployed: "https://lychee-cobbler-11074.herokuapp.com/",
+    source_code: "https://github.com/Arnette110/Caching-Budgets",
+  },
+  {
+    id: 11,
+    title: "Employee DireActory",
+    image: "/assets/images/2020-04-16 20_24_25-Employee-Directory.png",
+    alt: "employee directory app",
+    deployed: "https://arnette110.github.io/Employee-DireActory/",
+    source_code: "https://github.com/Arnette110/Employee-DireActory",
+  },
+];
+
+export default Items
