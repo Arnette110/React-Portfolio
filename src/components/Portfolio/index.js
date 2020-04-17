562e6853d2ceacbe0e26b42947a1e6227b5a15d5
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-16 22:27:29 -0600

    added portfolio card component

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+
+function PortfolioCard(props) {
+  return (
+    <div className="columns is-multiline">
+      {props.Items.map((items) => (
+        <div className="column is-4" key={items.id}>
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                <span>{items.title}</span>
+              </p>
+            </header>
+            <div className="card-content">
+              <figure className="image is-16by9">
+                <img src={items.image} alt={items.alt} />
+              </figure>
+            </div>
+            <footer className="card-footer">
+              <a
+                href={items.deployed}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="card-footer-item"
+              >
+                Deployed
+              </a>
+              <a
+                href={items.source_code}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="card-footer-item"
+              >
+                Source Code
+              </a>
+            </footer>
+          </div>
+        </div>
+      )).reverse()}
+    </div>
+  );
+}
+export default PortfolioCard;
