faf900e5037e89eea78d610eb2075d80f9cbe00c
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-16 22:25:50 -0600

    added footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import "./styles.css"
+
+export default class index extends Component {
+    render() {
+        return (
+          <div>
+            <footer className="footer" id="footerContent">
+              <div className="container has-text-centered">
+                <p>Christopher Ellis</p>
+                <i className="fa fa-copyright    "> 2020</i>
+              </div>
+            </footer>
+          </div>
+        );
+    }
+}
