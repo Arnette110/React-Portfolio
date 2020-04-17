af906364dbcec6c25c26ac0be6a63adf87c66c8a
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-16 22:23:22 -0600

    added routes for navigation, and nav burger logic for mobile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer"
 
 
 function App() {
+
+  useEffect(()=> {
+    document.title= "Chris Ellis Portfolio";
+    (function () {
+      var burger = document.querySelector(".burger");
+      var menu = document.querySelector("#" + burger.dataset.target);
+      burger.addEventListener("click", function () {
+        burger.classList.toggle("is-active");
+        menu.classList.toggle("is-active");
+      });
+    })();
+  })
+  
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
