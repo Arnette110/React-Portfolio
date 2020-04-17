0925481692ea046fe0b3bfaf9929c70b9058d794
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-16 22:29:08 -0600

    imported portfolio component into portfolio page and portfolio items from utils

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PortfolioCard from "../components/Portfolio";
+import Items from "../utils/portfolio";
+
+export default function Portfolio() {
+  return (
+    <section className="section is-medium" id="portfolio">
+      <div className="container">
+        <div className="heading" id="portfolioContainer">
+          <h2 className="title is-2" id="portfolioTitle">
+            portfolio
+          </h2>
+          <h4 className="subtitle is-5" id="portfolioSub">
+            my latest works
+          </h4>
+          <br />
+        </div>
+        <PortfolioCard Items={Items} />
+      </div>
+    </section>
+  );
+}
