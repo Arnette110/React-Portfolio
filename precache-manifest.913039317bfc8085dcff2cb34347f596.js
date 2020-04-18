40f0ec02aa5c4bbec6b2414ac92bfd7e1c149baa
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-17 20:36:40 -0600

    Updates

---
+++ precache-manifest.913039317bfc8085dcff2cb34347f596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5eca1ca8e088e383acb1009127451b",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "6f198dad6429fb53dd20",
+    "url": "/React-Portfolio/static/css/main.69dbc1c4.chunk.css"
+  },
+  {
+    "revision": "5494d4709d19d545d169",
+    "url": "/React-Portfolio/static/js/2.32515fae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.32515fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f198dad6429fb53dd20",
+    "url": "/React-Portfolio/static/js/main.f284261f.chunk.js"
+  },
+  {
+    "revision": "0fbff940b3028eaa15fc",
+    "url": "/React-Portfolio/static/js/runtime-main.844e07ae.js"
+  }
+]);
