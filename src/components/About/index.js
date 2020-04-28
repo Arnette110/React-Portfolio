db537b0b1631e93ebf97cbc412b5e856fc514fdc
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-04-27 18:34:37 -0600

    updated resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,28 +3,28 @@ import "./styles.css";
 
 export default function About() {
   return (
-    <section className="section is-medium" id="about">
-      <div className="container">
-        <div className="section-heading has-text-centered">
-          <h2 className="title is-2" id="aboutHead">
+    <section className='section is-medium' id='about'>
+      <div className='container'>
+        <div className='section-heading has-text-centered'>
+          <h2 className='title is-2' id='aboutHead'>
             About
           </h2>
           <br />
         </div>
-        <div className="columns" id="aboutContainer">
-          <div className="column is-one-fifth">
-            <figure className="image">
+        <div className='columns' id='aboutContainer'>
+          <div className='column is-one-fifth'>
+            <figure className='image'>
               <img
-                src={process.env.PUBLIC_URL + "/assets/images/ChrisEllis.jpg"}
-                alt="christopher ellis"
+                src={process.env.PUBLIC_URL + '/assets/images/ChrisEllis.jpg'}
+                alt='christopher ellis'
               />
             </figure>
           </div>
 
-          <div className="column is-two-thirds">
-            <div className="content is-medium">
-              <article id="aboutContent">
-                <p className="is-size-3">
+          <div className='column is-two-thirds'>
+            <div className='content is-medium'>
+              <article id='aboutContent'>
+                <p className='is-size-3'>
                   <strong>Hello! </strong> My name is Chris.
                 </p>
                 <p>
@@ -32,13 +32,13 @@ export default function About() {
                   background in customer service.
                 </p>
                 <p>
-                  I am proficient in{" "}
+                  I am proficient in{' '}
                   <strong>
-                    {" "}
+                    {' '}
                     HTML5, CSS3 (Bootstrap and Bulma), JavaScript/ES6/jQuery,
                     AJAX/axios, Node, Express, Handlebars, SQL/MySQL/Sequelize,
                     MongoDB/Mongoose/Robo3T, Git
-                  </strong>{" "}
+                  </strong>{' '}
                   and
                   <strong> React</strong>.
                 </p>
@@ -71,21 +71,20 @@ export default function About() {
           </div>
         </div>{" "} */}
         <a
-          className="button is-medium is-link is-outlined "
-          id="resume"
+          className='button is-medium is-link is-outlined '
+          id='resume'
           href={
             process.env.PUBLIC_URL +
-            "/assets/resume/CHRISTOPHER ELLIS_Resume.doc.pdf"
+            '/assets/resume/resumeCHRISTOPHER ELLIS_Resume.doc (2).pdf'
           }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <i className="far fa-2x fa-file-pdf"></i>
+          target='_blank'
+          rel='noopener noreferrer'>
+          <span className='icon'>
+            <i className='far fa-2x fa-file-pdf'></i>
           </span>
           <span>My Resume'</span>
         </a>
       </div>
     </section>
-  );
+  )
 }
