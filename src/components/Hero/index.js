0e4a02812a22e925d31c9263654c9ed0d38fed0e
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-05-18 18:40:34 -0600

    removed unused class

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,34 +1,27 @@
 import React from 'react'
-import "./styles.css"
+import './styles.css'
 
 export default function Hero() {
-    return (
-      <section className="hero is-fullheight-with-navbar has-background">
-        <img
-          className="hero-background is-transparent"
-          src={
-            process.env.PUBLIC_URL +
-            "/assets/images/green-pine-trees-covered-with-fogs-under-white-sky-during-167699.jpg"
-            
-          }
-          alt="background"
-        />
-
-        <div
-          className="hero-body"
-          id="heroBody"
-        >
-          <div className="container">
-            <h1 className="title is-1" id="heroTitle">
-              Christopher Ellis
-            </h1>
-            <h2 className="subtitle" id="heroSub">
-              full stack web developer
-            </h2>
-          </div>
+  return (
+    <section className='hero is-fullheight-with-navbar has-background'>
+      <img
+        className='hero-background'
+        src={
+          process.env.PUBLIC_URL +
+          '/assets/images/green-pine-trees-covered-with-fogs-under-white-sky-during-167699.jpg'
+        }
+        alt='background'
+      />
+      <div className='hero-body' id='heroBody'>
+        <div className='container'>
+          <h1 className='title is-1' id='heroTitle'>
+            Christopher Ellis
+          </h1>
+          <h2 className='subtitle' id='heroSub'>
+            full stack web developer
+          </h2>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  )
 }
-
-
