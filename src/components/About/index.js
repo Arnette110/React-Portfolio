9908f8ba1d4570f0eef8825d7c4acab569e98480
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-05-26 21:17:55 -0600

    added/edited about me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,11 +5,11 @@ export default function About() {
   return (
     <section className='section is-medium' id='about'>
       <div className='container'>
-        <div className='section-heading has-text-centered'>
+        <div className='heading has-text-centered'>
           <h2 className='title is-2' id='aboutHead'>
             About
           </h2>
-          <br />
+          <hr style={{ backgroundColor: '#363636' }} />
         </div>
         <div className='columns' id='aboutContainer'>
           <div className='column is-one-fifth'>
@@ -28,23 +28,32 @@ export default function About() {
                   <strong>Hello! </strong> My name is Chris.
                 </p>
                 <p>
-                  I am a <strong>full stack</strong> web developer with a
-                  background in customer service.
+                  <strong>Full stack</strong> web developer with a Bachelor's in
+                  International Studies with minors in Business and German who has a
+                  customer service and art background, culminating in an eye for
+                  design and detail, and focus on creating functional user
+                  experiences.
                 </p>
                 <p>
-                  I am proficient in{' '}
+                  Newly acquired skills in{' '}
+                  <strong>MongoDB, Express.js, React.js</strong> and{' '}
+                  <strong>Node.js</strong> as a recipient of a certificate in
+                  Full Stack Web Development from the University of Utah.
+                  <strong> Passionate</strong> about creating well designed apps
+                  with an emphasis on usability and aesthetic. Also proficient
+                  in{' '}
                   <strong>
                     {' '}
-                    MongoDB/Mongoose/Robo3T, Express, Node, React, HTML5, CSS3
-                    (Bootstrap and Bulma), JavaScript/ES6/jQuery, AJAX/axios,
-                    Handlebars, SQL/MySQL/Sequelize
+                    HTML5, CSS3 (Bootstrap and Bulma), JavaScript/ES6/jQuery,
+                    AJAX/axios, Handlebars, SQL/MySQL/Sequelize
                   </strong>{' '}
                   and
                   <strong> Git</strong>.
                 </p>
                 <p>
-                  When I'm not coding I enjoy painting, snowboarding, golfing
-                  and traveling.
+                  When I'm not coding I enjoy skiing/snowboarding, golfing and
+                  traveling with my wife. It's also like playing fetch and
+                  tug-o-war with my two dogs.
                 </p>
               </article>
             </div>
